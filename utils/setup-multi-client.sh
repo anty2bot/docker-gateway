1e23bdf99be1ddc8dae5908b150d5ff05787e53f
dot <179361261+anty2bot@users.noreply.github.com>
2025-07-31 20:43:41 +0800

    update to support multi client [2/2]
    add xray support

--- utils/setup-multi-client.sh
+++ utils/setup-multi-client.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Author: Dot(anty2bot)
 # Date: 2025-07-31
-# Description: Shell script to manage multiple proxy clients (V2Fly)
+# Description: Shell script to manage multiple proxy clients (V2Fly, Xray)
 
 readonly SCRIPT_PATH=$(dirname "$0")
 readonly CLIENT="$1"
@@ -9,10 +9,18 @@ readonly SHA256_DIR="$SCRIPT_PATH/sha256"
 
 case "$CLIENT" in
   "v2fly")
+    CONFIG_FILE="/etc/v2fly/config.json"
     read -r IMAGE IMAGE_ID < "$SHA256_DIR/v2fly.sha256"
     DOCKER_IMAGE="${IMAGE:-v2fly/v2fly-core:v5.16.1}"
     DOCKER_IMAGE_ID="${IMAGE_ID:-sha256:d1c717b3cc8c7602fdb89a886d0c7fc0cf8c1d973501101d5f5e86f1ec6dcccf}"
-    CONFIG_FILE="/etc/v2fly/config.json"
+    DOCKER_ARGS="run -c $CONFIG_FILE"
+    ;;
+  "xray")
+    CONFIG_FILE="/etc/xray/config.json"
+    read -r IMAGE IMAGE_ID < "$SHA256_DIR/xray.sha256"
+    DOCKER_IMAGE="${IMAGE:-teddysun/xray:25.7.26}"
+    DOCKER_IMAGE_ID="${IMAGE_ID:-sha256:5ca9e4c01ed0d42437d709403b531f14fac10cb84fcd49e5307b9f72eb09a8ea}"
+    DOCKER_ARGS=""
     ;;
   *)
     echo "Unsupported client: $CLIENT"
@@ -21,7 +29,7 @@ case "$CLIENT" in
 esac
 shift
 
-readonly DOCKER_IMAGE DOCKER_IMAGE_ID CONFIG_FILE
+readonly CONFIG_FILE DOCKER_IMAGE DOCKER_IMAGE_ID DOCKER_ARGS
 
 setup_image() {
   if docker inspect "$DOCKER_IMAGE" > /dev/null 2>&1; then
@@ -58,7 +66,7 @@ setup_container() {
   readonly local FLAG="$3"
   readonly local TEMP_CONFIG_FILE="$4"
 
-  readonly local CONTAINER_NAME="$CLIENT-$NAME"
+  readonly local CONTAINER_NAME="proxy-$NAME"
   readonly local CONTAINER_HTTP_PORT="$PORT"
   readonly local CONTAINER_SOCK_PORT="$(($PORT + 1))"
 
@@ -90,7 +98,7 @@ setup_container() {
     -p $CONTAINER_HTTP_PORT:$CONTAINER_HTTP_PORT \
     -p $CONTAINER_SOCK_PORT:$CONTAINER_SOCK_PORT \
     $DOCKER_IMAGE \
-    run -c $CONFIG_FILE
+    $DOCKER_ARGS
 
   rm $CONFIG_FILE
 }
